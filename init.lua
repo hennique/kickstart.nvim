59438fcb51ebc32e344d0066d1754b7ef8c91c07
hennique <henriquevelososribeiro@gmail.com>
2025-09-07 12:41:14 -0300

    feat: Add Java autocomplete support

--- init.lua
+++ init.lua
@@ -692,7 +692,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        -- jdtls = {}, -- Java autocomplete
         html = {},
         omnisharp = {}, -- C# autocomplete
         cssls = {},
