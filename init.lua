a9fb1cec6f6be612a4dfc991de0aebe3c25abae8
hennique <henriquevelososribeiro@gmail.com>
2025-09-07 12:46:22 -0300

    chore: Add comment to clangd lsp

--- init.lua
+++ init.lua
@@ -692,6 +692,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        -- clangd = {}, -- C and C++ autocomplete
         -- jdtls = {}, -- Java autocomplete
         html = {},
         omnisharp = {}, -- C# autocomplete
