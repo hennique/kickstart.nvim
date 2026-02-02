94da611896e7269d0fbddd72dd648056980db4cb
hennique <henriquevelososribeiro@gmail.com>
2026-02-02 09:59:33 -0300

    feat: add bash support

--- init.lua
+++ init.lua
@@ -734,6 +734,7 @@ require('lazy').setup({
         cssls = {},
         -- gopls = {},
         pyright = {},
+        bashls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
