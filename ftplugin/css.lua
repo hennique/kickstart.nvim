0fa8d1cccf1e90d64d478b5866cac3ddb63a1205
hennique <henriquevelososribeiro@gmail.com>
2026-02-21 19:38:49 -0300

    feat: remove stylelint's fix on save

--- ftplugin/css.lua
+++ ftplugin/css.lua
@@ -21,14 +21,3 @@ if vim.fn.filereadable(stylelint_config_file) == 0 then
   vim.cmd.terminal { args = { 'npm', 'add', '--prefix', current_path, '-D', 'stylelint', 'stylelint-config-standard' } }
   vim.cmd 'e!'
 end
-
-local stylelint_group = vim.api.nvim_create_augroup('stylelint-fix-on-save', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePre', {
-  group = stylelint_group,
-  callback = function()
-    local stylelint_bin = vim.fn.stdpath 'data' .. '/mason/packages/stylelint/node_modules/stylelint/bin/stylelint.mjs'
-
-    vim.system { stylelint_bin, '--fix', current_path }
-    vim.cmd 'e!'
-  end,
-})
