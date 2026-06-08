70db655f14ce1200987dfc82b16d57132ac39d4e
hennique <henriquevelososribeiro@gmail.com>
2026-06-08 14:49:10 -0300

    feat: add twee support

--- init.lua
+++ init.lua
@@ -580,6 +580,9 @@ do
     fix_on_save = 1,
   }
 
+  vim.pack.add { gh 'hennique/twee.nvim' } -- Twee support
+  require('twee').setup {}
+
   --  This function gets run when an LSP attaches to a particular buffer.
   --    That is to say, every time a new file is opened that is associated with
   --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
